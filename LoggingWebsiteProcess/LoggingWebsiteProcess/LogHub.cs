fa16ced561b6ff8cb11ec5dc5724fd54aa6ea4ad
AkankshaInvatu <akanksha.mishra@invatu.com>
2024-02-12 14:09:34 +0530

    committed code

---
+++ LoggingWebsiteProcess/LoggingWebsiteProcess/LogHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LoggingWebsiteProcess
+{
+    public class LogHub :Hub
+    {
+        public async Task SendProcessInfo(string processInfo)
+        {
+            // Broadcast the received process info to all connected clients
+            await Clients.All.SendAsync("ReceiveProcessInfo", processInfo);
+        }
+    }
+}
---
+++ LoggingWebsiteProcess/LoggingWebsiteProcess/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoggingWebsiteProcess.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
