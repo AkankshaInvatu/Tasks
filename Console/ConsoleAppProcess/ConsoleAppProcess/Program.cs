6d3110766896ab05c91fc526d544debdaef601f1
AkankshaInvatu <akanksha.mishra@invatu.com>
2024-02-12 16:38:00 +0530

    commiited code

---
+++ Console/ConsoleAppProcess/ConsoleAppProcess/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("List of Running Applications:");
+
+        var runningApplications = Process.GetProcesses()
+            .Where(process =>
+                !string.IsNullOrEmpty(process.MainWindowTitle) &&
+                !string.IsNullOrWhiteSpace(process.MainWindowTitle) && 
+                process.MainModule != null) 
+            .Select(process => process.ProcessName);
+
+        foreach (var application in runningApplications)
+        {
+            Console.WriteLine(application);
+        }
+
+        Console.ReadLine();
+    }
+}
