7ae1d57c1435a7c10b617b4a9c5fda6bd7e44523
AkankshaInvatu <akanksha.mishra@invatu.com>
2024-02-12 11:25:30 +0530

    comiiteed code

---
+++ Console/ConsoleDuplicateRemoveProcess/ConsoleDuplicateRemoveProcess/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("List of Running Processes:");
+
+        var runningProcesses = Process.GetProcesses()
+            .GroupBy(process => process.ProcessName)
+            .Select(group => group.First());
+
+        foreach (var process in runningProcesses)
+        {
+            Console.WriteLine($"{process.ProcessName} (ID: {process.Id})");
+        }
+
+        Console.ReadLine();
+    }
+}
