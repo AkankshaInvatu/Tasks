d691ddae986173216451b4c683f4796ca629e0a1
AkankshaInvatu <akanksha.mishra@invatu.com>
2024-02-14 09:41:04 +0530

    committed code of data manually display on website

---
+++ signalr2/LoggingWebsite/LoggingWebsite/LogHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LoggingWebsite
+{
+    public class LogHub :Hub
+    {
+        public async Task SendMessage( string message)
+        {
+            // Broadcast the received message to all connected clients
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
