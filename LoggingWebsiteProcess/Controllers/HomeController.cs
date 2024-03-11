305ce59127adb66866bf46414dd8387a6fbcc621
AkankshaInvatu <akanksha.mishra@invatu.com>
2024-03-11 14:09:44 +0530

    committed code

--- LoggingWebsiteProcess/Controllers/HomeController.cs
+++ LoggingWebsiteProcess/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace LoggingWebsiteProcess.Controllers
         {
             return View();
         }
-
+        public IActionResult GeneralLogs()
+        {
+            return View();  
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
